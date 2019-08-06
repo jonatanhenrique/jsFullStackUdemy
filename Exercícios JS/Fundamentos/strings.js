cdbff8c47791f95763c5e1ee4992b80822a78abd
Jonatan Henrique <henrique.oliveirajonatan@gmail.com>
2019-08-05 21:34:20 -0300

    Commit Inicial: JS Fundamentos parte 1

---
+++ Exercícios JS/Fundamentos/strings.js
@@ -0,0 +1,11 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4)); // letra no indice 4
+
+console.log(escola.charCodeAt(3)); // codigo ASC
+console.log(escola.indexOf("o")); // n do indice
+console.log(escola.substring(1)); // forma string a partir do indice 1 
+console.log("Escola ".concat(escola).concat("!!!"));
+console.log(escola.replace(3, "e")); 
+
+console.log("Ana,Maria,Pedro".split(","));
---
+++ Exercícios JS/Fundamentos/templateString.js
@@ -0,0 +1,13 @@
+const nome = 'Rebeca';
+const concatena = 'Olá' + nome + '!';
+
+const template = `Olá
+${nome}
+!`;
+
+console.log(template);
+
+console.log(`1 + 1 =  ${1+1}`);
+
+const up = texto => texto.toUpperCase(); // up recebe texto e converte para upper
+console.log (`Ei... ${up('cuidado')}!`);
